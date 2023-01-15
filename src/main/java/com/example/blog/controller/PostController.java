55c5f86b7b9d6d39dabac975bc071dad2abcf2ea
Baamki <sevenbumki@gmail.com>
2023-01-15 22:07:18 +0900

    POST 데이터 콘텐츠 타입

--- src/main/java/com/example/blog/controller/PostController.java
+++ src/main/java/com/example/blog/controller/PostController.java
@@ -1,8 +1,15 @@
 package com.example.blog.controller;
 
 
+import com.example.blog.request.PostCreate;
+import lombok.extern.java.Log;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 
 //SSR : JSP, thymeleaf, Mustache
@@ -10,13 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
 //SRA : Vue, React, API
 //  자바스크립트 + API(JSON)
 
+@Slf4j
 @RestController
 public class PostController {
 
-    @GetMapping("/v1/posts")
+    @GetMapping("/hello")
     public String get()
     {
         return "Hello World";
     }
+    // HTTP 메소드 :  GET POST PUT PATCH DELETE OPTIONS HEAD TRACE CONNECT
+    // 글 등록을 하는데 POST 메소드를 사용할 것
+
+    @PostMapping("/posts")
+    public String post(@RequestBody PostCreate params)
+    {
+        log.info("params={}",params.toString());
+        return "Hello World";
+    }
+
+
+
+
+
+
 
 }
--- src/test/java/com/example/blog/controller/PostControllerTest.java
+++ src/test/java/com/example/blog/controller/PostControllerTest.java
@@ -3,23 +3,54 @@ package com.example.blog.controller;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@WebMvcTest             ///해당 어노테이션 없이 MockMVC 의존성 주입을 하면 불가능
 class PostControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+    //perform()
+    //요청을 전송하는 역할을 합니다. 결과로 ResultActions 객체를 받으며
+    // ResultActions 객체는 리턴 값을 검증하고 확인할 수 있는 andExcpect() 메소드를 제공해줍니다.
+    // https://shinsunyoung.tistory.com/52
 
 
     @Test
-    @DisplayName("/Posts 요청 시 Hello World를 출력하기")
-    void test()
-    {
-        // expected
-//        mockMvc.perform(MockMvcRequestBuilders.get("/posts")).andExpect(Mock)
+    @DisplayName("/Post 요청 시 Hello World를 출력하는지")
+    void test() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.get("/posts")).andExpect(MockMvcResultMatchers.status().isOk()).
+                andExpect(MockMvcResultMatchers.content().string("Hello World"))
+                .andDo(MockMvcResultHandlers.print());
     }
 
+
+    @Test
+    @DisplayName("post 요청 시 Hello World 출력하는지 확인한다.")
+    void postTest() throws Exception {
+        // 글 제목
+        // 글 내용
+
+        // expected
+        mockMvc.perform(MockMvcRequestBuilders.post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\" : \"제목입니다.\", \"content\":\"내용입니다.\"}")
+                )   //applicataion/json 형식으로 보낸다.
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello World"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
+
+
 }
+
+
+/// MockMVC란 아주 기초적인 테스트 코드로서 가짜 요청을 만들어 더욱 테스트 하기 쉽게 사용하는 것.
