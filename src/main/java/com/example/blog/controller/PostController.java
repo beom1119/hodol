de06b2425c18d4184467304eb09f18c3a97a0bcc
Baamki <sevenbumki@gmail.com>
2023-01-15 19:41:54 +0900

    "Git"

--- src/main/java/com/example/blog/controller/PostController.java
+++ src/main/java/com/example/blog/controller/PostController.java
@@ -4,6 +4,12 @@ package com.example.blog.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+//SSR : JSP, thymeleaf, Mustache
+//  서버에서 랜더링을 진행 후 랜더링 해준다.
+//SRA : Vue, React, API
+//  자바스크립트 + API(JSON)
+
 @RestController
 public class PostController {
 
