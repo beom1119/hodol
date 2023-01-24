af96c5ebc31c892b94401fa99646fb86654aa27c
Baamki <sevenbumki@gmail.com>
2023-01-24 20:45:11 +0900

    POST 데이터 콘텐츠 타입

--- src/main/java/com/example/blog/controller/PostController.java
+++ src/main/java/com/example/blog/controller/PostController.java
@@ -24,16 +24,36 @@ public class PostController {
     @GetMapping("/hello")
     public String get()
     {
-        return "Hello World";
+        return "haha";
     }
     // HTTP 메소드 :  GET POST PUT PATCH DELETE OPTIONS HEAD TRACE CONNECT
     // 글 등록을 하는데 POST 메소드를 사용할 것
 
     @PostMapping("/posts")
-    public String post(@RequestBody PostCreate params)
-    {
+    public String post(@RequestBody PostCreate params) throws Exception {
         log.info("params={}",params.toString());
+        String title = params.title;
+        String content = params.content;
+
+
+        /// 이런 식의 에러 확인은 너무 노가다
+        /// 1.힘들다. 2. 무언가 3번 이상 반복잗업을 할떄 내가 뭔가 잘못하고 있는건 아닐지 의심한다.
+        /// 3.누락될 수 있다. 4. 생각보다 검증해야할게 많다.(중요)
+        /// 5.꼼꼼하지 않을 수 있다.
+        if (title ==null || title.equals(""))
+        {
+            throw new Exception("타이틀 값이 없어요~!");
+        }
+        else if (content ==null ||content.equals(""))
+        {
+            throw new Exception("내용이 없어요");
+        }
+
+
+
         return "Hello World";
+
+
     }
 
 
--- src/test/java/com/example/blog/controller/PostControllerTest.java
+++ src/test/java/com/example/blog/controller/PostControllerTest.java
@@ -22,19 +22,9 @@ class PostControllerTest {
     // ResultActions 객체는 리턴 값을 검증하고 확인할 수 있는 andExcpect() 메소드를 제공해줍니다.
     // https://shinsunyoung.tistory.com/52
 
-
-    @Test
-    @DisplayName("/Post 요청 시 Hello World를 출력하는지")
-    void test() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.get("/posts")).andExpect(MockMvcResultMatchers.status().isOk()).
-                andExpect(MockMvcResultMatchers.content().string("Hello World"))
-                .andDo(MockMvcResultHandlers.print());
-    }
-
-
     @Test
     @DisplayName("post 요청 시 Hello World 출력하는지 확인한다.")
-    void postTest() throws Exception {
+    void test() throws Exception {
         // 글 제목
         // 글 내용
 
@@ -47,6 +37,25 @@ class PostControllerTest {
                 .andExpect(MockMvcResultMatchers.content().string("Hello World"))
                 .andDo(MockMvcResultHandlers.print());
     }
+
+
+
+    @Test
+    @DisplayName("post 요청 TITLE 값은 필수다.")
+    void test2() throws Exception {
+        // 글 제목
+        // 글 내용
+
+        // expected
+        mockMvc.perform(MockMvcRequestBuilders.post("/posts")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("{\"title\" : \"\", \"content\":\"내용입니다.\"}")
+                )   //applicataion/json 형식으로 보낸다.
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("Hello World"))
+                .andDo(MockMvcResultHandlers.print());
+    }
+
 
 
 
